d0e9b2b1f5dee483fe612cdb7445ce650d85d81d
Mohammed Shifreen <mshifreen@gmail.com>
2026-04-15 10:46:10 +0530

    * AggregateRoot base class added with unit tests. * Did composer update as well.

---
+++ src/Base/Domain/Entity/AggregateRoot.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * The file is part of the "webifycms/domain", WebifyCMS extension package.
+ *
+ * @see https://webifycms.com/extension/domain
+ *
+ * @copyright Copyright (c) 2023 WebifyCMS
+ * @license https://webifycms.com/extension/domain/license
+ * @author Mohammed Shifreen <mshifreen@gmail.com>
+ */
+declare(strict_types=1);
+
+namespace Webifycms\Domain\Base\Domain\Entity;
+
+use Webifycms\Domain\Base\Domain\Event\DomainEventInterface;
+
+/**
+ * The aggregate root base class.
+ */
+abstract class AggregateRoot
+{
+	/**
+	 * The recorded domain events list.
+	 *
+	 * @var DomainEventInterface[]
+	 */
+	private array $domainEvents = [];
+
+	/**
+	 * Record an event.
+	 */
+	public function recordDomainEvent(DomainEventInterface $event): void
+	{
+		$this->domainEvents[] = $event;
+	}
+
+	/**
+	 * Get domain events.
+	 *
+	 * @return DomainEventInterface[]
+	 */
+	public function getDomainEvents(): array
+	{
+		return $this->domainEvents;
+	}
+
+	/**
+	 * Clear all the domain events.
+	 *
+	 * @return DomainEventInterface[]
+	 */
+	public function releaseDomainEvents(): array
+	{
+		$domainEvents       = $this->domainEvents;
+		$this->domainEvents = [];
+
+		return $domainEvents;
+	}
+}
